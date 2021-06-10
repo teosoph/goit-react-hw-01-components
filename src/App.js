c22cd7eb3757257bd063f1039bb67749381ea274
teosoph <39537001+teosoph@users.noreply.github.com>
2021-06-11 01:57:37 +0300

    JS__React__HW__modul-1__task-3

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import user from "./Components/Profile/user.json";
 import Statistics from "./Components/Statistics/Statistics";
 import statisticalData from "./Components/Statistics/statistical-data.json";
 
+import FriendList from "./Components/FriendList/Friends";
+import friends from "./Components/FriendList/friends.json";
+
 const App = () => {
   return (
     <div>
       <h1 style={{ color: "white", textShadow: "1px 1px 2px rgb(0, 0, 0)" }}>
         PROFILE {user.name}
       </h1>
-
       <Profile
         name={user.name}
         tag={user.tag}
@@ -18,9 +20,9 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
+      <FriendList friends={friends} />
     </div>
   );
 };
